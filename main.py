32bd0594eb769276fd4547aa891c6fb6b7d7b76e
dumar <dumarzhev@gmail.com>
2023-03-04 23:33:05 +1000

    chess field 0.2

--- main.py
+++ main.py
@@ -6,20 +6,33 @@ def run():
     screen = pygame.display.set_mode((900, 900))
     pygame.display.set_caption("Шахматы")
     bg_color = (180, 60, 20)
-    clock = pygame.time.Clock()
     FPS = 10
-    screen.fill(bg_color)
     length_width = 8
     size_field = 100
-    colors_field = ((200, 200, 200), (10, 10, 10))
+    colors_field = ((240, 240, 240), (10, 10, 10))
+    TRS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    screen.fill(bg_color)
+    FNT = pygame.font.Font(pygame.font.get_default_font(), 24)
+    clock = pygame.time.Clock()
 
     even = length_width % 2 == 0
     color_index = 1 if even else 0
-    for y in range(length_width):
+    for y in range(length_width):# отрисовка поля
         for x in range(length_width):
             pygame.draw.rect(screen, colors_field[color_index], (x*size_field, y*size_field, size_field, size_field))
             color_index ^= True
         color_index = color_index ^ True if even else color_index
+
+    for i in range(0, length_width):# отрисовка цифр и букв
+        letter = FNT.render(TRS[i], True, (255, 255, 255))
+        numders = FNT.render(str(length_width - i), True, (255, 255, 255))
+        screen.blit(letter, (i * size_field + size_field // 2 - size_field * 0.05,
+                    size_field * length_width + (size_field // 4))
+                    )
+        screen.blit(numders, (size_field * length_width + (size_field // 4),
+                    i * size_field + size_field // 2 - size_field * 0.05)
+                    )
 
 
     while True:
